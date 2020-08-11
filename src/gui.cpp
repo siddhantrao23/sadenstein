4270680f0fa76be823e11e6cf49bae689ecc065d
Siddhant Rao <raosiddhant99@gmail.com>
2020-08-11 14:08:14 +0530

    add new titan sprites

--- src/gui.cpp
+++ src/gui.cpp
@@ -22,9 +22,9 @@ int main() {
             {1.834, 8.765, 4, 0},
             {5.323, 5.365, 1, 0},
             {14.32, 13.36, 3, 0},
-            {4.123, 10.76, 5, 0} },
+            {4.123, 10.76, 0, 0} },
         Texture("../textures/walltext.bmp", SDL_PIXELFORMAT_ABGR8888),  // textures for the walls
-        Texture("../textures/monsters2.bmp", SDL_PIXELFORMAT_ABGR8888)}; // textures for the monsters
+        Texture("../textures/char2.bmp", SDL_PIXELFORMAT_ABGR8888)}; // textures for the monsters
     if (!gs.tex_walls.count || !gs.tex_monst.count) {
         std::cerr << "Error: Failed to load textures" << std::endl;
         return -1;
