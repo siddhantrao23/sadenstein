b46753f1e42842f25dfd2875c419ac14c70145ef
Siddhant Rao <raosiddhant99@gmail.com>
2020-08-13 23:37:49 +0530

    start menu implementation

---
+++ src/menu.h
@@ -0,0 +1,14 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "SDL.h"
+
+enum ActiveMenu {None, Main, Pause, GameOver, DrawMap, Win, Help};
+enum Volume {NoSound, LowSound, NormalSound}
+
+class Menu {
+    public:
+        bool hover(uint8_t button_id)
+}
+
+#endif
